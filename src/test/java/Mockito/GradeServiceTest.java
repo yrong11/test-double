3c5d2a395435200b6b7ec1773cb90082ac36733c
yurong <rong.yu@thoughtworks.com>
2020-08-04 21:20:00 +0800

    fix: rename mock grade system

--- src/test/java/Mockito/GradeServiceTest.java
+++ src/test/java/Mockito/GradeServiceTest.java
@@ -16,18 +16,18 @@ import static org.mockito.Mockito.mockitoSession;
 class GradeServiceTest {
 
 
-    GradeSystem mockGradeSystem;
+    GradeSystem GradeSystem;
     GradeService gradeService;
 
     @BeforeEach
     void setup(){
-        mockGradeSystem = mock(GradeSystem.class);
-        gradeService = new GradeService(mockGradeSystem);
+        GradeSystem = mock(GradeSystem.class);
+        gradeService = new GradeService(GradeSystem);
     }
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        Mockito.when(mockGradeSystem.gradesFor(1)).thenReturn(Arrays.asList(80.0, 90.0, 100.0));
+        Mockito.when(GradeSystem.gradesFor(1)).thenReturn(Arrays.asList(80.0, 90.0, 100.0));
         assertEquals(90, gradeService.calculateAverageGrades(1));
     }
 
